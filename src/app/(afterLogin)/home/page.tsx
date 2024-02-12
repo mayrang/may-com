c2bfc08540c59ce2c6301dba9a310c9be02e027b
mayrang <pkss0626@naver.com>
2024-02-13 00:25:28 +0900

    page routing

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AfterHomePage() {
+  return <div></div>;
+}
---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default function LoginPage() {
+  return redirect("/i/flow/login");
+}
