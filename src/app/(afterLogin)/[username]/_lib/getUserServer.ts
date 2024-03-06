ed8433d3fc21b21a67728074eb7f64d099e59e4a
mayrang <pkss0626@naver.com>
2024-03-06 13:03:09 +0900

    getUserServer 변경

--- src/app/(afterLogin)/[username]/_lib/getUserServer.ts
+++ src/app/(afterLogin)/[username]/_lib/getUserServer.ts
@@ -2,7 +2,7 @@ import { User } from "@/model/User";
 import { QueryFunction } from "@tanstack/react-query";
 import { cookies } from "next/headers";
 
-export const getUserServer: QueryFunction<User, [_1: string, username: string]> = async ({ queryKey }) => {
+export const getUserServer = async ({ queryKey }: {queryKey: [string, string]}) => {
   const [_1, username] = queryKey;
   const res = await fetch(`http://localhost:9090/api/users/${username}`, {
     next: {
